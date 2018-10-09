e80d1f033364e015288ce4bdc0b3d14fffe0cc34
vsebastianvc <42979187+vsebastianvc@users.noreply.github.com>
2018-10-08 21:00:47 -0500

    Initial Commit

---
+++ src/model/dataaccess/DataAccess.java
@@ -0,0 +1,14 @@
+package model.dataaccess;
+
+import java.util.HashMap;
+
+import model.domain.Book;
+import model.domain.LibraryMember;
+import model.domain.User;
+
+public interface DataAccess {
+	public HashMap<String,Book> readBooksMap();
+	public HashMap<String,User> readUserMap();
+	public HashMap<String, LibraryMember> readMemberMap();
+	public void saveNewMember(LibraryMember member);
+}
