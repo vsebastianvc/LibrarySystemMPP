656a4460f10b25d6e4be974e68c25eafe525cf6d
DELL <DELL@10.10.11.36>
2018-10-09 15:15:48 -0500

    addBook and checkout screens ready

--- src/control/SystemController.java
+++ src/control/SystemController.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 
 import javafx.animation.FadeTransition;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
@@ -18,31 +21,72 @@ public class SystemController {
 
 	@FXML  // fx:id="menuCheckOut"
 	private MenuItem menuCheckOut;
-	
 	@FXML  // fx:id="menuCheckIn"
 	private MenuItem menuCheckIn;
-	
 	@FXML  // fx:id="menuQueryOverdue"
 	private MenuItem menuQueryOverdue;
-	
 	@FXML  // fx:id="printCheckoutRecord"
 	private MenuItem printCheckoutRecord;
-	
 	@FXML  // fx:id="menuSystemExit"
 	private MenuItem menuSystemExit;
-	
 	@FXML  // fx:id="menuNewMember"
 	private MenuItem menuNewMember;
-
 	@FXML  // fx:id="menuNewBook"
 	private MenuItem menuNewBook;
-	
-	@FXML  // fx:id="newBookCopy"
+    @FXML  // fx:id="newBookCopy"
 	private MenuItem menuBookCopy;
+
+	//New book screen items
+	@FXML  // fx:id="fieldNewBookISBN"
+	private TextField fieldNewBookISBN;	
+	@FXML  // fx:id="fieldNewBookTitle"
+	private TextField fieldNewBookTitle;
+	@FXML  // fx:id="fieldNewBookAuthors"
+	private TextField fieldNewBookAuthors;
+	@FXML  // fx:id="choiceNewBookMaxCheckout"
+	private ChoiceBox<String> choiceNewBookMaxCheckout;
+	@FXML  // fx:id="fieldNewBookNumCopies"
+	private TextField fieldNewBookNumCopies;
 	
+
+	@FXML  // fx:id="createNewBook"
+	private Button btnCreateNewBook;
+	
+    @FXML
+    void createNewBook(ActionEvent event) {
+    	
+    	System.out.printf("Add book with: ISBN: %s, Title: %s, Authors: %s, Max Checkout: %s, num of copies: %s \n",
+    			fieldNewBookISBN.getText(), fieldNewBookTitle.getText(), fieldNewBookAuthors.getText(), choiceNewBookMaxCheckout.getValue(), 
+    			fieldNewBookNumCopies.getText());
+    }
+    
+	
+	private void initMaxCheckout() {
+		ObservableList<String> listMaxDays = FXCollections.observableArrayList();
+		listMaxDays.removeAll(listMaxDays);
+		String days7="7";
+		String days12="12";
+		listMaxDays.addAll(days7,days12);
+		//choiceNewBookMaxCheckout.getItems().addAll(listMaxDays);
+	}
+	
+
+    @FXML
+    void newBookFired(ActionEvent event) {
+    	System.out.println("New Book");
+		try {
+			AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("addBook.fxml"));
+			this.contentPanel.getChildren().clear();
+			this.contentPanel.getChildren().add(page);
+			System.out.println("Agregado el panel");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
 	//New member's screen items
-	
-	@FXML  // fx:id="newMemberId"
+	@FXML  // fx:id="fieldNewMemberId"
 	private TextField fieldNewMemberId;
 	@FXML  // fx:id="newFirstName"
 	private TextField fieldNewFirstName;
@@ -64,15 +108,54 @@ public class SystemController {
 	
     @FXML
     void createNewMember(ActionEvent event) {
-    	String strMemberId;
     	
-    	strMemberId = fieldNewMemberId.getText();
     	System.out.printf("Add member with: ID: %s, First name: %s, Last name: %s, Tel number: %s, Street: %s, State: %s, City: %s, Zip: %s \n",
     			fieldNewMemberId.getText(), fieldNewFirstName.getText(), fieldNewLastName.getText(), fieldNewTelNumber.getText(), 
     			fieldNewStreet.getText(), fieldNewState.getText(), fieldNewCity.getText(), fieldNewZip.getText());
     }
 
-	@FXML //  fx:id="newIssue"
+	//New book's copy screen items
+	@FXML  // fx:id="fieldNewCopyISBN"
+	private TextField fieldNewCopyISBN;
+	
+	@FXML  // fx:id="btnCreateNewBookCopy"
+	private Button btnCreateNewBookCopy;
+	
+    @FXML
+    void createNewBookCopy(ActionEvent event) {
+
+    	System.out.printf("Add book copy with: ISBN: %s \n",fieldNewCopyISBN.getText());
+    }
+    
+    @FXML
+    void newBookCopyFired(ActionEvent event) {
+    	System.out.println("New Book Copy");
+		try {
+			AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("addBookCopy.fxml"));
+			this.contentPanel.getChildren().clear();
+			this.contentPanel.getChildren().add(page);
+			System.out.println("Agregado el panel");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+    }
+   }
+    
+	//Checkout screen items
+	@FXML  // fx:id="fieldCheckoutISBN"
+	private TextField fieldCheckoutISBN;
+	@FXML  // fx:id="fieldCheckoutMemberId"
+	private TextField fieldCheckoutMemberId;
+	
+	@FXML  // fx:id="btnCheckout"
+	private Button btnCheckout;
+	
+    @FXML
+    void checkoutBook(ActionEvent event) {
+    	System.out.printf("Checkout book with: ISBN: %s for member id: %s \n",fieldCheckoutISBN.getText(),fieldCheckoutMemberId.getText());
+    }
+    
+    @FXML //  fx:id="newIssue"
     private Button btnNewIssue; // Value injected by FXMLLoader
 
     @FXML //  fx:id="saveIssue"
@@ -86,7 +169,7 @@ public class SystemController {
      */
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        
+    	initMaxCheckout();
     }
 
     /**
@@ -199,34 +282,6 @@ public class SystemController {
 		}
     }
     
-    @FXML
-    void newBookFired(ActionEvent event) {
-    	System.out.println("New Book");
-		try {
-			AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("addBook.fxml"));
-			this.contentPanel.getChildren().clear();
-			this.contentPanel.getChildren().add(page);
-			System.out.println("Agregado el panel");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-    
-    
-    @FXML
-    void newBookCopyFired(ActionEvent event) {
-    	System.out.println("New Book Copy");
-		try {
-			AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("addBookCopy.fxml"));
-			this.contentPanel.getChildren().clear();
-			this.contentPanel.getChildren().add(page);
-			System.out.println("Agregado el panel");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-    }
-   }
     
     @FXML
     void printCheckoutRecordFired(ActionEvent event) {
