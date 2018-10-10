c13f489772fe0df3cc5d06e4f215bdd44bf681ab
DELL <DELL@WINDOWS-JV6C7PB>
2018-10-09 21:30:54 -0500

    Exception class

---
+++ src/util/ValException.java
@@ -0,0 +1,12 @@
+package util;
+
+@SuppressWarnings("serial")
+final public class ValException extends Exception {
+	public ValException() {
+		super();
+	}
+	public ValException(String msg) {
+		super(msg);
+	}
+}
+
