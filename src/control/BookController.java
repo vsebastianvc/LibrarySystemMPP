1777c377ccd12aaedeebc68283f07adb25f3d192
DELL <DELL@WINDOWS-JV6C7PB>
2018-10-10 20:43:28 -0500

    Grid for checkoutrecord

--- src/control/BookController.java
+++ src/control/BookController.java
@@ -1,13 +1,18 @@
 package control;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import javafx.scene.control.Alert.AlertType;
 import model.dataaccess.DataAccess;
 import model.dataaccess.DataAccessFacade;
--- src/control/LoginController.java
+++ src/control/LoginController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -74,4 +75,5 @@ public class LoginController extends Application {
 		}
 
 	}
+	
 }
